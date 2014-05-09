ed2c8a8049fa58fe1bdbedbfbd0e2ec54c645cb3
Ruchir Shukla <ruchir@bizzappdev.com>
2014-05-09 11:29:13 +0530

    copy module from old repo

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+    "name": "Make product internal reference unique",
+    "version": "1.0",
+    "depends": ["product"],
+    "author": "Bizzappdev",
+    'website': 'http://www.bizzappdev.com',
+    "category": "Inventory Control",
+    "description": """Makes product internal reference (default_code) a unique field 
+    """,
+    "init_xml": [],
+    'update_xml': [],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+
+}
